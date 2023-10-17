e36fe9bb3b06f9e1e99c9fb466cfd1d7d1f9a328
VictorYanovych2000 <yanovich2000@ukr.net>
2023-10-17 15:58:49 +0300

    Second_task_table_beta 2

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationFormComponent } from "./registration-form/registration-form.component";
 import { LoginFormComponent } from "./login-form/login-form.component";
+import { TableComponent} from "./table/table.component";
 
 const routes: Routes = [
   { path: 'register', component: RegistrationFormComponent},
   { path: 'login', component: LoginFormComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'table', component: TableComponent},
+  { path: '', redirectTo: '/table', pathMatch: 'full'},
 ];
 
 @NgModule({
---
+++ my-app/src/app/table/table-data.ts
@@ -0,0 +1,6 @@
+export const TABLE_DATA = [
+  { column1: 'APP123', column2: 'AppLes', column3: 'kg', column4: 'Value4', column5: 'In stock', column6: 'Gif' },
+  { column1: 'TOM53', column2: 'Tomatos', column3: 'box', column4: 'Value4', column5: 'In stock', column6: 'Gif'},
+  { column1: 'CUC997', column2: 'Cucumbers', column3: 'pcs', column4: 'Value4', column5: 'Our of stock', column6: 'Gif'},
+
+];
